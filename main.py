7ab2b03ebdbca8955cbdceca46c243d32df0f1b2
Christopher McLean <71855991+Iago-7@users.noreply.github.com>
2023-08-03 16:20:42 -0700

    "Added main and implemented screen."

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Screen
+
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+
+
+screen = Screen()
+screen.screensize(SCREEN_WIDTH, SCREEN_HEIGHT)
+screen.bgcolor("black")
+
+
+screen.exitonclick()
